7a050bc6d649fd743be3ce6f65aadd403428c1b0
ash3n <33986672+ash3n@users.noreply.github.com>
2022-01-27 15:57:27 -0500

    Fix precache manifest

---
+++ precache-manifest.c1ab545bd4a64276a6afb9d63d88ebba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d311cb0404253babe177f3d524721d31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c37002d7cebe7c77e85",
+    "url": "/static/js/2.3b9119a2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3b9119a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4f7ff0b0c68dc60036",
+    "url": "/static/js/main.ad647945.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
