7cd2ec332ae582ccb1ce03bb8a2790ffbdb32451
ash3n <33986672+ash3n@users.noreply.github.com>
2022-02-08 15:11:09 -0500

    Another rewording

--- precache-manifest.4d38c81c65db51abdb8a600ed57f7d05.js
+++ precache-manifest.4d38c81c65db51abdb8a600ed57f7d05.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e8d619f36f6613fc7b099e511f3c2e6",
+    "revision": "e7f7b14328509ee972a8bd66404832f7",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3b9119a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3327ce60330354e1a0f7",
-    "url": "/static/js/main.e3a26880.chunk.js"
+    "revision": "566d403a7f1b045078d0",
+    "url": "/static/js/main.5913a100.chunk.js"
   },
   {
     "revision": "f139b624154a37532bb8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64169928ffe2f6c748c4a34e0d12837b.js"
+  "/precache-manifest.4d38c81c65db51abdb8a600ed57f7d05.js"
 );
 
 self.addEventListener('message', (event) => {
